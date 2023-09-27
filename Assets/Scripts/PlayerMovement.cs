38dd432f878cf51dd2fb5672bcf4f6769f498c18
Aman Menda <aman.menda@epitech.eu>
2023-09-27 23:01:41 +0100

    basic player animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public Animator animator;
+    private SpriteRenderer sr;
+
+    [SerializeField] private float dirX = 0;
+    [SerializeField] private float moveSpeed = 12.0f;
+    [SerializeField] private float jumpForce = 20.0f;
+    [SerializeField] private bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
+        UpdateAnimation();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            animator.SetBool("isJumping", false);
+        }
+        else
+        {
+            animator.SetBool("isJumping", true);
+        }
+    }
+    void UpdateAnimation()
+    {
+        Jump();
+        if (dirX > 0f)
+        {
+            sr.flipX = false;
+            animator.SetBool("isRunning", true);
+        }
+        else if (dirX < 0f)
+        {
+            sr.flipX = true;
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
